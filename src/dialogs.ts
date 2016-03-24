7f5b8e05d082dac3d63215aed10ebc3b089561a0
Erik Lieben <erik@codetistic.com>
2016-03-24 20:58:05 +0100

    added doc for toastservice

--- src/dialogs.ts
+++ src/dialogs.ts
@@ -1,5 +1,20 @@
-export class Dialogs {   
-    displayCustomHTMLToast() {       
-        Materialize.toast($('<span>I am toast content</span>'), 5000);
-    }   
+import {ToastService} from "aurelia-materialize-css";
+
+export class Dialogs {  
+  
+    public toast() {     
+      ToastService.show("I am a toast", 4000);
+    }
+   
+    public displayCustomHTMLToast() {       
+      ToastService.show($('<span>I am toast content</span>'), 5000);
+    }
+    
+    public toastWithCallback() {     
+      ToastService.show("I am a toast", 4000).then(() => ToastService.show("Your toast was dismissed", 1000, "blue darken-3"));
+    }
+    
+    public styledToast() {     
+      ToastService.show("I am a toast", 4000, "rounded");
+    }
 }
--- src/prism/index.ts
+++ src/prism/index.ts
@@ -1,10 +1,14 @@
 import "@eriklieben/prismjs";
+import "@eriklieben/prismjs/components/prism-typescript.min.js";
 import "@eriklieben/prismjs/themes/prism.css!";
 import "@eriklieben/prismjs/plugins/line-numbers/prism-line-numbers";
 import "@eriklieben/prismjs/plugins/line-numbers/prism-line-numbers.css!";
 
 import "@eriklieben/prismjs/plugins/show-language/prism-show-language";
 import "@eriklieben/prismjs/plugins/show-language/prism-show-language.css!";
+
+import "@eriklieben/prismjs/plugins/previewer-color/prism-previewer-color";
+import "@eriklieben/prismjs/plugins/previewer-color/prism-previewer-color.css!";
 
 export function configure(config) {
   config.globalResources('./prismElement');
